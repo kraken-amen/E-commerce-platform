d5c276044397a25d81c140525c818c5837b4f29d
امان الله الورتاني <krakenamen@gmail.com>
2026-01-23 15:27:08 +0100

    navbar in menu_list

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,5 +7,5 @@ import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
 )
