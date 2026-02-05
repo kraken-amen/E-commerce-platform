efd86b58433ecd79d8fcd8ef54e8b0703b5a47d3
امان الله الورتاني <krakenamen@gmail.com>
2026-02-05 16:44:40 +0100

    in model

---
+++ backend/controllers/userController.js
@@ -0,0 +1,8 @@
+//route for user login
+const loginUser = async (req, res) => {
+    
+}
+//route for user register
+const registerUser = async (req, res) => {
+    
+}
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 // app config
 const app = express();
 const PORT = process.env.PORT || 4000;
 connectDB();
+connectCloudinary();
 //middelwares
 app.use(cors());
 app.use(express.json());
